dcec60f93ead9a8b4a59a7fd4a15456f0be3a476
suhassamuel <suhas.sahaj1@gmail.com>
2022-06-21 23:13:15 -0400

    locator added for cart class

--- src/main/java/com/qa/opencart/pages/Cart.java
+++ src/main/java/com/qa/opencart/pages/Cart.java
@@ -1,10 +1,15 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class Cart {
+	
+	private By cart = By.id("cart");
 	
 	public void getcart()
 	{
-		System.out.println("cart method");
+		
+		System.out.println("cart method"+cart);
 	}
 
 }
